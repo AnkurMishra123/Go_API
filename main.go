c5d40b9aad52b92210e79980c562c686747b184e
AnkurMishra123 <ankurmishra0043@gmail.com>
2023-12-13 11:02:10 +0530

    With Post functionality

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	// "strconv"
+
+	"github.com/gorilla/mux"
+
+)
+
+var profiles []Profile = []Profile{}
+
+type User struct{
+	FirstName string `json:"firstName"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+}
+
+type Profile struct{
+	Department  string `json:"department"`
+	Designation string `json:"designation"`
+	Employee 	User `json:"employee"`
+}
+
+func getAllProfiles(q http.ResponseWriter, r *http.Request)
+{
+	q.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(q).Encode(profiles)
+}
+func addItem(q http.ResponseWriter, r *http.Request) {
+	var newProfile Profile
+	json.NewDecoder(r.Body).Decode(&newProfile)
+
+	q.Header().Set("Content-Type", "application/json")
+
+	profiles = append(profiles, newProfile)
+
+	json.NewEncoder(q).Encode(profiles)
+}
+
+func main() {
+	router:= mux.NewRouter()
+
+	router.HandleFunc("/profiles", addItem).Methods("POST")
+	router.HandleFunc("/profiles", getAllProfiles).Methods("GET")
+	http.ListenAndServe(":5000", router)
+}
